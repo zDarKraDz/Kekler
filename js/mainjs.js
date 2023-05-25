1558435abaa1729550c606ed66923b6489eeba4d
zDarKraDz <134440655+zDarKraDz@users.noreply.github.com>
2023-05-25 16:40:08 +0300

    Update mainjs.js

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,3 +1,42 @@
-window.scroll({
-	right: 0
-})
+/*анимация появления блоков*/
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('element-show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
+
+
+/*Закрепление шапки*/
+let header = document.querySelector('.header'),
+	headerH = document.querySelector('.header').clientHeight;
+
+document.onscroll = function(){
+	let scroll = window.scrollY;
+
+	if(scroll > headerH){
+		header.classList.add('fixed');
+		document.body.style.paddingTop = headerH + 139+ 'px';
+	}else{
+		header.classList.remove('fixed');
+		document.body.removeAttribute('style');
+	}
+}
+
+/*закрытие блока*/
+var krestik = document.getElementById('krestik');
+var adv = document.getElementById('header-line');
+document.onclick = function(){
+    krestik.style.display = 'none';
+    adv.style.display = 'none';
+};
